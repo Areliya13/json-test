8014843e0c11eb88536d546ee3310adc286bda9a
Areliya13 <sharipovatamara@gmail.com>
2022-07-08 01:36:24 +0300

    Conditional statements and Data types Practical task

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Hello world! I am json.')
+const str = '20';
+const num = 10;
+const a = false;
+const b = true;
+
+console.log('str + num = ' + (str + num));
+console.log('str + a = ' + str + a);
+console.log('str + b = ' + str + b);
+console.log('b + num = ' + b + num);
+
+console.log('num + str = ' + num + str);
+console.log('a + str = ' + a + str);
+console.log('num + b = ' + num + b);
+
+console.log('str * num = ' + str * num);
+console.log('str * a = ' + str * a);
+console.log('str * b = ' + str * b);
+console.log('b * num = ' + b * num);
+
+console.log('num * str = ' + num * str);
+console.log('a * str = ' + a * str);
+console.log('num * a = ' + num * a);
+
+console.log('str / num = ' + str / num);
+console.log('str / a = ' + str / a);
+console.log('str / b = ' + str / b);
+console.log('b / num = ' + b / num);
+
+console.log('num / str = ' + num / str);
+console.log('a / str = ' + a / str);
+console.log('num / a = ' + num / a);
+
+console.log('String(num) = ' + String(num));
+console.log('String(a) = ' + String(a));
+console.log('String(b) = ' + String(b));
+
+console.log('Number(str) = ' + Number(str));
+console.log('Number(a) = ' + Number(a));
+console.log('Number(b) = ' + Number(b));
+
+console.log('Boolean(str) = ' + Boolean(str));
+console.log('Boolean(num) = ' + Boolean(num));
+console.log('Boolean(\'\') = ' + Boolean(''));
+console.log('Boolean(0) = ' + Boolean(0));
