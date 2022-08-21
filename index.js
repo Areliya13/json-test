b08c6e21d96d9017fe5124c2457b3326382667f8
Areliya13 <sharipovatamara@gmail.com>
2022-08-21 07:54:56 +0300

    module 7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello world! I am json.')
+const testString = 'ahb acb aeb aeeb adcb axeb'
+const re = /a.b/gs
+console.log(testString.match(re))
+
+const testString2 = '2 + 3 223 2223'
+const re2 = /2 \+ 3/
+console.log(testString2.match(re2))
+
+const date = new Date()
+const day = date.getDate()
+const month = date.getMonth()
+const year = date.getFullYear()
+
+console.log(`Day: ${day}`)
+console.log(`Month (from 0): ${month}`)
+console.log(`Month (used to): ${month + 1}`)
+console.log(`Year: ${year}`)
